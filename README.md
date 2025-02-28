0fe8239e33bffacff8e8a1db640bf694c159f796
Spencer Vore <69659970+spencervore-dev@users.noreply.github.com>
2025-02-28 15:26:08 -0700

    demo inspired by training video

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# concurrent_programming_demo2
+Feb 2025
+Author: Spencer Vore
+
+This repository is my recreation from memory of the multi-threaded examples show in the Concurrent and Parallel Programming in Python course: 
+https://www.udemy.com/course/concurrent-and-parallel-programming-in-python/
+
+Wanted to get some hands on experience with some of the basic python multithreadng and parallel libraries.
+
+
+
---
+++ single_threaded.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+from pprint import pprint
+import time
+import yfinance as yf
+
+
+def get_stock_symbols():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    raw_html = requests.get(url, timeout=600)
+    soup = BeautifulSoup(raw_html.text, 'lxml')
+    table = soup.find(id='constituents')
+    table_rows = table.find_all('tr')
+    symbols = []
+    for table_row in table_rows[1:]:
+        symbol = table_row.find('td').text.strip('\n')
+        symbols.append(symbol)
+    return symbols
+
+
+def get_price(symbol):
+    # We will use yfinance library, unlike in demo. Yahoo finance is set up to return a 423 
+    # Too many requests error when trying to just do a simple requests.get(). Don't know how to get around
+    # this so switching to library that packages this logic for me.
+    data = yf.Ticker(symbol)
+    price = data.info.get('currentPrice')
+    print(f"{symbol}: ${price}")  
+
+    # slow down API calls a bit
+    time.sleep(2)
+     
+
+def main():
+    tik = time.perf_counter()
+    companies = get_stock_symbols()
+    # print(companies)
+
+    for symbol in companies:
+        get_price(symbol)
+    tok = time.perf_counter()
+    print(f"TOTAL RUNTIME (secs): {tok-tik}")
+
+
+if __name__ == "__main__":
+    main()
